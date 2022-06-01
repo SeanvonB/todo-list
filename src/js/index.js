89ef4690c7dc9462c81bd6a10aac8fe0729da17d
SeanvonB <42122062+SeanvonB@users.noreply.github.com>
2022-06-01 13:45:17 -0700

    Add fontawesome package

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import { userInterface } from "./userInterface";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
 
 document.addEventListener("DOMContentLoaded", userInterface.init);
